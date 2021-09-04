458a5bfeda416166a23acabaca90c9c04d3bd118
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-09-04 23:31:40 +0600

    default event with click event

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function Home() {
+    const handleClick=(e)=>{
+        console.log('hello',e.target)
+    }
     return (
         <div className="home">
-            <h2>Homepage</h2>            
+            <h2>Homepage</h2>      
+            <button onClick={handleClick}> Click here</button>
+
         </div>
     )
 }
