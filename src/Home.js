eb37721a533d6d2fa5204d12f34d3b8b76ce8b24
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-09-06 12:34:43 +0600

    reusing comp and filtering out

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,15 @@ import BlogList from './BlogList'
 
 function Home() {
     const [blogs,setBlogs]=useState([
-        {title: 'My new Website', body:'Lorem ipsum....', author:'Mandio',id:1},
+        {title: 'My new Website', body:'Lorem ipsum....', author:'Mario',id:1},
         {title: 'Blog & Art by me', body:'Lorem ipsum....', author:'Gomez',id:2},
-        {title: 'My Epic travel guide', body:'Lorem ipsum....', author:'Mrinio',id:3}
+        {title: 'My Epic travel guide', body:'Lorem ipsum....', author:'Mario',id:3}
     ])
 
     return (
         <div className="home">  
             <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs.filter((blog)=>blog.author==='Mario')} title="Mario's Blogs"/>
         </div>
     )
 }
