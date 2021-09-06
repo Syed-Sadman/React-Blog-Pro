643136a3f349c44dbed8511d7394b3fd6ae1ea44
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-09-06 13:01:01 +0600

    Passing functions as props

--- src/BlogList.js
+++ src/BlogList.js
@@ -3,6 +3,7 @@ import React from 'react'
 function BlogList(props) {      //another way is destructuring props like {blogs,title}
     const blogs=props.blogs
     const title=props.title
+    const handleDelete=props.handleDelete
     return (
         <div className="blog-list">
                 <h2> {title}</h2>
@@ -11,6 +12,7 @@ function BlogList(props) {      //another way is destructuring props like {blogs
                     <div className="blog-preview" key={blog.id}>
                     <h2> {blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={()=>handleDelete(blog.id)}> Delete blog</button>
                     </div>
                     
                 ))}            
