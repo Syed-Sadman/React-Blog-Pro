2798bc2ae6206beb94d2e81eb7d79366770b9e1d
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-10-05 23:23:15 +0600

    input form

--- src/Contact.js
+++ src/Contact.js
@@ -3,8 +3,27 @@
 
 const Contact=()=>{
     return(
-        <div>
+        <div className="contact">
             <h1> Add new blogs</h1>
+            <form>
+                <label>Blog title : </label>
+                <input 
+                type="text"
+                required
+                />
+                <label>Blog body</label>
+                <textarea
+                required
+                >
+
+                </textarea>
+                <label>Blog Author </label>
+                <select>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     )
 }
