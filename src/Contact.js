a348857f03c8951140cfbc7f0e941c547e14ba32
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-10-06 22:38:15 +0600

    Controlled inputs in forms

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,11 @@
+import { useState } from "react/cjs/react.development";
 
 
 
 const Contact=()=>{
+    const [title,setTitle]=useState('');
+    const [body,setBody]=useState('');
+    const [author,setAuthor]=useState('mario');    
     return(
         <div className="contact">
             <h1> Add new blogs</h1>
@@ -10,19 +14,27 @@ const Contact=()=>{
                 <input 
                 type="text"
                 required
+                value={title}
+                onChange={(e)=>setTitle(e.target.value)}
                 />
                 <label>Blog body</label>
                 <textarea
                 required
+                value={body}
+                onChange={(e)=>setBody(e.target.value)}
                 >
 
                 </textarea>
                 <label>Blog Author </label>
-                <select>
+                <select 
+                value={author}
+                onChange={(e)=>setAuthor(e.target.value)}>
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
                 <button>Add Blog</button>
+                <p>{title} and {body}</p>
+                <p>{author}</p>
             </form>
         </div>
     )
