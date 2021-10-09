3bc20cd0455e4062bcaab3083b3921c964c792d7
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-10-09 19:04:44 +0600

    deleting blogs that were added

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 
@@ -6,6 +6,15 @@ import useFetch from "./useFetch";
 const BlogDetails=()=>{
     const {id}=useParams();
     const { error,isPending, data: blog}=useFetch('http://localhost:8000/blogs/' + id);
+    const handleDelete=()=>{
+        fetch('http://localhost:8000/blogs/' + blog.id,{
+            method: 'DELETE'
+        })
+        .then(()=>{
+            hist.push('/')
+        })
+    }
+    const hist=useHistory()
     return(
         <div className="blog-details">
             {isPending && <div> Loading......</div>}
@@ -15,6 +24,7 @@ const BlogDetails=()=>{
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleDelete}>Delete this Blog</button>
                 </article>
             )}
         </div>
